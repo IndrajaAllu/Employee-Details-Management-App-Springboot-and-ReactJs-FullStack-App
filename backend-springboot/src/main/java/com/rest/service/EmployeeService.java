0537401ca4036915cfce1e707ffb355d158ba590
IndrajaAllu <indrajaallu@gmail.com>
2024-06-21 16:05:43 +0530

    first commit

---
+++ backend-springboot/src/main/java/com/rest/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.rest.service;
+
+import java.util.List;
+
+import com.rest.model.Employee;
+
+public interface EmployeeService 
+{
+	    //add employee//
+		public Employee saveEmployee(Employee employee);
+		 
+		//get employee//
+		public List<Employee>getAllEmp();
+		
+		//update employee//
+		public Employee updateEmployee(long id,Employee employee);
+		
+		//get one record//
+		public Employee getOneEmployee(long id);
+		
+		//delete one record//
+		public void deleteOneEmployee(long id);
+
+
+}
